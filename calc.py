f4e65a7f356654a73be6f8b41ef8c5e550f93afa
sahanakshendre <sahanashendre644@gmail.com>
2026-03-04 16:39:20 +0530

    initial commit

---
+++ calc.py
@@ -0,0 +1,49 @@
+from utils.advancedCalculator import AdvancedCalculator
+
+def main():
+    while True:
+        print("\nCalculator")
+        choice = input("Q => Quit or press ENTER to continue: ")
+
+        if choice.upper() == 'Q':
+            print("Exiting the calculator. Goodbye!")
+            exit()
+
+        first_number = float(input("Enter the first number: "))
+        operation = input("Enter the operation (+, -, *, /, ^, sqrt): ") 
+        operation = operation.lower()
+
+        if operation != 'sqrt':
+            second_number = float(input("Enter the second number: "))
+        else:
+            second_number = None
+    
+
+        calculator = AdvancedCalculator(first_number, operation, second_number)
+
+        if calculator.operation == '+':
+            result = calculator.add()
+
+        elif calculator.operation == '-':
+            result = calculator.subtract()
+
+        elif calculator.operation == '*':
+            result = calculator.multiply()
+
+        elif calculator.operation == '/':
+            result = calculator.divide()
+
+        elif calculator.operation == '^':
+            result = calculator.power() 
+
+        elif calculator.operation == 'sqrt':
+            result = calculator.square_root()
+
+        else:
+            result = "Error: Invalid operation."
+
+        print(f"Result: {result}")
+
+
+if __name__ == "__main__":    
+    main()
---
+++ utils/advancedCalculator.py
@@ -0,0 +1,10 @@
+from utils.calculator import Calculator
+
+class AdvancedCalculator(Calculator): #inherit from Calculator
+    def power(self):
+        return self.first_number ** self.second_number
+    
+    def square_root(self):
+        if self.first_number < 0:
+            return "Error: Cannot calculate square root of a negative number."
+        return self.first_number ** 0.5
